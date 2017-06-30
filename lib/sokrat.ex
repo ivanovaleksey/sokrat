04f52c6633255dd737ee986dc085c3a78a4b327f
Aleksey Ivanov <ialexxei@gmail.com>
2017-06-30 20:55:24 +0300

    Generate new application

---
+++ lib/sokrat.ex
@@ -0,0 +1,18 @@
+defmodule Sokrat do
+  @moduledoc """
+  Documentation for Sokrat.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sokrat.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sokrat_test.exs
@@ -0,0 +1,8 @@
+defmodule SokratTest do
+  use ExUnit.Case
+  doctest Sokrat
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
