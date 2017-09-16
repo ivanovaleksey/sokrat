66167e70c6c3402d4be4b643d832d7f97f3ef163
Aleksey Ivanov <ialexxei@gmail.com>
2017-09-16 13:41:52 +0300

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sokrat.Mixfile do
 
   def project do
     [app: :sokrat,
-     version: "1.0.0",
+     version: "1.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
